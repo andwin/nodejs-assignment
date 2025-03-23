ed8f407403c25a15481f81d78e6d8b91cfe9b852
Anders Windelhed <anders.windelhed@tv4.se>
2025-03-23 19:39:27 +0100

    Lint fix - extracting to separate variable

--- controllers/package.controller.ts
+++ controllers/package.controller.ts
@@ -1,10 +1,12 @@
 import {type Request, type Response} from 'express';
 import PackageService from '../services/package.service';
 
-export default {
+const packageController = {
 	async getAll(_: Request, response: Response) {
 		const packages = await PackageService.getAll();
 
 		response.send({packages});
 	},
 };
+
+export default packageController;
