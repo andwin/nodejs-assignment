7fcae8631ea6757f9aeaeb94a899a5e987045ec8
Anders Windelhed <anders.windelhed@tv4.se>
2025-03-23 19:28:03 +0100

    Disabling lint rule for now

--- services/price.service.ts
+++ services/price.service.ts
@@ -3,6 +3,7 @@ import {Price} from '../models/price';
 
 const priceService = {
 	// You may want to use this empty method 🤔
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	async getPriceHistory() {},
 };
 
