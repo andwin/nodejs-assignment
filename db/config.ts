1704317e2fa7386c4283179543e8372247e76f6f
Anders Windelhed <anders.windelhed@tv4.se>
2025-03-23 22:31:52 +0100

    Using env variable to enable sequelize logging

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,10 @@
 import {Sequelize} from 'sequelize';
 
+// eslint-disable-next-line n/prefer-global/process
+const logging = process.env.LOGGING === 'true';
+
 const sequelizeConnection = new Sequelize({
+	logging,
 	dialect: 'sqlite',
 	storage: './db/sqlite.db',
 });
