1e781456e348ba4f1772b8ab69fa859335df68bb
Anders Windelhed <anders.windelhed@tv4.se>
2025-03-23 19:31:28 +0100

    Disabling lint rule

--- routes/package.routes.ts
+++ routes/package.routes.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import packageController from '../controllers/package.controller';
 
+// eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.get('/', packageController.getAll);
