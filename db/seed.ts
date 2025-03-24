c3b70ec6dd4ac6481ab5bd2d36b4a9dcee32f504
Anders Windelhed <anders.windelhed@tv4.se>
2025-03-24 22:24:06 +0100

    Adding more data to data base seed file

--- db/seed.ts
+++ db/seed.ts
@@ -15,20 +15,38 @@ export const seedDatabase = async () => {
 	const premium = (await Package.findOne({where: {name: 'premium'}}))!;
 
 	await Price.bulkCreate([
-		{priceCents: 5000, packageId: basic.id},
-		{priceCents: 10_000, packageId: basic.id},
+		{
+			priceCents: 5000, packageId: basic.id, municipality: 'Göteborg', priceDate: new Date('2020-01-01'),
+		},
+		{
+			priceCents: 10_000, packageId: basic.id, municipality: 'Stockholm', priceDate: new Date('2020-01-01'),
+		},
 	], {validate: true});
 
 	await Price.bulkCreate([
-		{priceCents: 19_990, packageId: plus.id},
-		{priceCents: 29_900, packageId: plus.id},
-		{priceCents: 39_900, packageId: plus.id},
+		{
+			priceCents: 19_990, packageId: plus.id, municipality: 'Göteborg', priceDate: new Date('2020-01-01'),
+		},
+		{
+			priceCents: 29_900, packageId: plus.id, municipality: 'Stockholm', priceDate: new Date('2020-01-01'),
+		},
+		{
+			priceCents: 39_900, packageId: plus.id, municipality: 'Stockholm', priceDate: new Date('2020-01-02'),
+		},
 	], {validate: true});
 
 	await Price.bulkCreate([
-		{priceCents: 55_000, packageId: premium.id},
-		{priceCents: 66_600, packageId: premium.id},
-		{priceCents: 77_700, packageId: premium.id},
-		{priceCents: 88_800, packageId: premium.id},
+		{
+			priceCents: 55_000, packageId: premium.id, municipality: 'Göteborg', priceDate: new Date('2020-01-01'),
+		},
+		{
+			priceCents: 66_600, packageId: premium.id, municipality: 'Stockholm', priceDate: new Date('2020-01-01'),
+		},
+		{
+			priceCents: 77_700, packageId: premium.id, municipality: 'Stockholm', priceDate: new Date('2020-01-02'),
+		},
+		{
+			priceCents: 88_800, packageId: premium.id, municipality: 'Stockholm', priceDate: new Date('2020-01-03'),
+		},
 	], {validate: true});
 };
